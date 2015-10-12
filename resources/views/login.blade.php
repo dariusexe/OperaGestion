d0d661f64989b171e3ffb9d39717fec5dcd4e3fe
dariusexe <darioinfor@gmail.com>
2015-10-13 00:12:09 +0200

    inital commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,32 @@
+@extends ('layouts.plane')
+@section ('body')
+<div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+            <br /><br /><br />
+               @section ('login_panel_title','Inicio de sesión')
+               @section ('login_panel_body')
+                        <form role="form">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Contraseña" name="password" type="password" value="">
+                                </div>
+                                <div class="checkbox">
+                                    <label>
+                                        <input name="remember" type="checkbox" value="Remember Me">Recordarme
+                                    </label>
+                                </div>
+                                <!-- Change this to a button or input when using this as a form -->
+                                <input type="submit" class="btn btn-lg btn-success btn-block">
+                            </fieldset>
+                        </form>
+                    
+                @endsection
+                @include('widgets.panel', array('as'=>'login', 'header'=>true))
+            </div>
+        </div>
+    </div>
+@stop
